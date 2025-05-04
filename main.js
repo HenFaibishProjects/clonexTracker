5e1e750fbe4b01fba3ece7440fa30336bf4271d0
Hen <bandel79@gmail.com>
2025-05-04 20:38:41 +0300

    fix

--- main.js
+++ main.js
@@ -1,30 +1,12 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = !app.isPackaged;
+import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import * as express from 'express';
+import {AppModule} from "./src/app.module";
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 700,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
 
-    if (isDev) {
-        win.loadURL('http://localhost:3000'); // Your NestJS + frontend server
-    } else {
-        win.loadFile(path.join(__dirname, 'dist', 'index.html'));
-    }
+    app.use(express.static(join(__dirname, '..', 'public')));
+    await app.listen(3000);
 }
-
-app.whenReady().then(() => {
-    createWindow();
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
+bootstrap();

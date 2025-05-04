0bf4626ab728353d0cd7a223d755a74a479bfb97
Hen <bandel79@gmail.com>
2025-05-04 20:41:03 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import {NestFactory} from "@nestjs/core";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);

64d913db2b4c2f2b42e5ddc8194879f3f77242e9
Hen <bandel79@gmail.com>
2025-05-05 21:29:01 +0300

    change db

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ClonexService } from './clonex.service';
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DB_HOST,
-            port: parseInt(process.env.DB_PORT!, 10),
+            port: parseInt(process.env.DB_PORT || '5432', 10),
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,

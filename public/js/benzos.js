32cbb49d529afe27b1beee9d2957376590d8c62c
henfa <HenFaibishProjects@users.noreply.github.com>
2025-06-15 19:23:13 +0300

    fix now button (#70)

--- public/js/benzos.js
+++ public/js/benzos.js
@@ -73,8 +73,13 @@ $(document).ready(function () {
 
     $('#nowBtn').click(function () {
         const now = new Date();
-        const formattedNow = now.toISOString().slice(0, 16);  // 'YYYY-MM-DDTHH:MM'
-        flatpickrInstance.setDate(formattedNow, true);
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const localDatetimeStr = `${year}-${month}-${day}T${hours}:${minutes}`;
+        flatpickrInstance.setDate(localDatetimeStr, true);
     });
 
     const token = localStorage.getItem('token');

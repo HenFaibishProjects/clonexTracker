23618ae669657a1b3317b88502badbbc4430ea96
lidasoftwarenet-hash <lidasoftwarenet@gmail.com>
2026-08-19 17:40:47 +0300

    Register news image enrichment service

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NewsService } from './news.service';
 import { NewsController } from './news.controller';
+import { NewsImageService } from './news-image.service';
 import { Feed } from './entities/feed.entity';
 import { NewsItem } from './entities/news-item.entity';
 import { Topic } from './entities/topic.entity';
@@ -11,7 +12,7 @@ import { Topic } from './entities/topic.entity';
     TypeOrmModule.forFeature([Feed, NewsItem, Topic]),
   ],
   controllers: [NewsController],
-  providers: [NewsService],
+  providers: [NewsService, NewsImageService],
   exports: [NewsService],
 })
 export class NewsModule {}
